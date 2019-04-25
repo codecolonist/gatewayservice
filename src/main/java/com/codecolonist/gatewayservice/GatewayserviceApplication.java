e005a457b86012c07995c4d6fb5e8b7bd5bd6030
Ramkishorerao Gujjeti <codecolonist@gmail.com>
2019-04-24 23:27:28 -0500

    adding zuul routes

--- src/main/java/com/codecolonist/gatewayservice/GatewayserviceApplication.java
+++ src/main/java/com/codecolonist/gatewayservice/GatewayserviceApplication.java
@@ -2,10 +2,12 @@ package com.codecolonist.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class GatewayserviceApplication {
 
 	public static void main(String[] args) {
